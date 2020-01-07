6a1b13ccf0f70e406adebd6ff1e0ab558055e41c
John Doe <nickgray0@gmail.com>
2020-01-07 21:05:54 +0800

    Update true_oop.py

--- true_oop.py
+++ true_oop.py
@@ -1,18 +1,19 @@
+import sys
 obj_state = 0
-symbols = {}
 
 def obj():
     global obj_state
     #for UFCS(Unified Function Call Syntax)
     def _getattr(self, func):
-        return lambda *args, **kw: symbols[func](self, *args, **kw)
+        module = sys.modules['__main__'] if hasattr(sys.modules['__main__'], func) else sys.modules[self.__module__]
+        return lambda *args, **kw: getattr(module, func)(self, *args, **kw)
     #to dynamically attach member functions
     def _setattr(self, attr, value):
         if callable(value):
             setattr(type(self), attr, value)
         else:
             self.__dict__[attr] = value
-    settings = dict(__getattr__ = _getattr, __setattr__=_setattr)
+    settings = {'__getattr__' : _getattr, '__setattr__' : _setattr}
     obj_class = type('obj'+str(obj_state), (object,), settings)
     obj_state += 1
     return obj_class()
